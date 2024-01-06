a43e1260928ad3a056b8de48ad6e92f182d98677
lucastandy <lucastitandy@gmail.com>
2024-01-06 14:46:10 -0300

    Criandp a rota visualizar e enviar parâmetro

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Instale todas as dependências indicado pelo pachage.json
 Comando para rodar o projeto
 ### node.app.js
 
+Comando que roda o projeto usando o nodemon
+### nodemon app.js
+
 Abrir o endereço no navegador para acessar a página inicial
 ### http://localhost:8090
 
--- app.js
+++ app.js
@@ -5,9 +5,28 @@ const express = require('express');
 // Chamando a função express
 const app = express();
 
-// Criando a primeira rota
+// Criando a rota listar
 app.get("/", (req, res) => {
+    // Retornando um texto como resposta
     res.send("Bem-vindo 2, Lucas Tandy!");
+});
+
+// Criação da rota visualizar
+app.get("/users/:id", (req, res) =>{
+    
+    // Usando a desestruturação para simplificar a atribuição do parâmetro
+    const {id} = req.params; // Ex.: http://localhost:8090/users/1
+
+    // Usando a desestruturação para simplificar a atribuição do parâmetro
+    const {sit} = req.query; // Ex.: http://localhost:8090/users/1?sit=2
+
+    return res.json({
+        id,
+        name: "Lucas",
+        email: "lucastitandy@gmail.com",
+        sit
+    });
+    // res.send(`Visualizar: ${id}`);
 });
 
 // Iniciando o servidor na porta 8090, criando a função utilizando o modelo Arrow function para retornar a mansagem de sucesso
